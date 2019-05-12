71c7aa24bb995dcf1f84237e88007032509f72fd
MikeFranco <mfranco_98@yahoo.com>
2019-05-12 13:17:18 -0500

    creating new frontend

---
+++ components/Home.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="hello">
+
+    <div class="barraBusqueda">
+      <h3>Ingrese el ID del pokemon:</h3>
+      <input style="color: black" type="text" v-model="mandoID" maxlength="3" @keyup.enter="sendID()">
+    </div>
+
+    <div class="cantidad">
+      <h3>¿Cuántos Pokemons quieres ver?</h3>
+      <input type="number" v-model="limit" maxlength="3" @keyup.enter="pokeInterval()">{{limit}}
+    </div>
+
+    <div class="pokealeatorio">
+
+      <div class="botonshowpoke"> <button style="color: black" @click="pokeRandom()">Show Pokemon Random</button>  </div>
+
+      <p v-if="isPokemonShown">
+        <br>Pokemon con ID aleatorio <br>
+        <img :src="pokemon.image"/>
+        <br>Un {{pokemon.name}} ha aparecido<br>
+        El id del pokemon es: {{pokemon.id}}<br>
+        ¡Cuidado!, {{pokemon.name}} ha usado: {{pokemon.move}}<br>
+      </p>
+    </div>
+
+    <div class="pokeID">
+
+      <p v-if="isPokemonSpec">
+        <br>Pokemon con ID Específico <br>
+      <img :src="pokemon2.image"/>
+        <br>Un {{pokemon2.name}} ha aparecido<br>
+        El id del {{pokemon2.name}} es: {{pokemon2.id}}<br>
+        ¡Cuidado!, {{pokemon2.name}} ha usado: {{pokemon2.move}}<br>
+      </p>
+    </div>
+
+    <div class="pokeLists">
+      <cards v-for="pokemon in pokemonList" :key="pokemon.id" :pokemon="pokemon"/>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import cards from '@/components/cards';
+
+export default {
+  name: 'HelloWorld',
+  data(){
+    return {
+      mandoID: '',
+      limit: '',
+      isPokemonShown: false,
+      isPokemonSpec: false,
+      pokemon: {},
+      pokemon2: {},
+      pokemonList: []
+    }
+  },
+  methods: {
+
+    async pokeRandom(){
+      this.isPokemonShown = true;
+       this.pokemon = this.getPokemonDataFromResponse (await axios.post('http://localhost:6001/verPokemones') );
+    },
+
+    getPokemonDataFromResponse(response){
+      // eslint-disable-next-line
+      console.log(response);
+      return response.data;
+    },
+    async sendID(){
+      this.isPokemonSpec = true;
+      (this.mandoID == '')
+        ? alert('Favor de ingresar un ID')
+        : (this.mandoID <= 802)
+          ? this.pokemon2 = this.getPokemonDataFromResponse( await axios.get('http://localhost:6001/pokemonEspec', { params:{ id : this.mandoID } }))
+          : alert(`El pokemon con id ${this.mandoID} no está registrado en la Pokedex`);
+    },
+
+    async pokeInterval(){
+      (this.limit == '')
+        ? alert('Favor de ingresar cuantos pokemones quieres ver')
+        : this.back()
+    },
+    async back(){
+      this.pokemonList = this.getPokemonDataFromResponse( await axios.get('http://localhost:6001/pokemonIntervalo', { params:{id: this.mandoID, limit: this.limit}}))
+    },
+
+  },
+  components:{
+    cards
+  }
+}
+</script>
+
+
+<style scoped>
+button {
+  text-align: center;
+  margin: 40px 0 0;
+  background-color: rgb(9, 173, 238);
+  border-radius: 10px;
+}
+p{
+  text-align:center;
+}
+.barraBusqueda{
+  text-align: start;
+  position: absolute;
+  top: 0px;
+  left: 350px;
+}
+.cantidad{
+  text-align: start;
+  position: absolute;
+  top: 0px;
+  left: 60%;
+}
+.botonshowpoke{
+  padding-top: 50px;
+}
+input{
+  border-radius: 15px;
+}
+.pokealeatorio{
+  position: absolute;
+  top: 150px;
+  left: 150px;
+}
+.pokeID{
+  margin-top: 128px;
+  position: absolute;
+  top: 150px;
+  left: 50%;
+}
+pokeLists{
+  position: center;
+  margin-top: 200px;
+
+}
+</style>
---
+++ components/PokeCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div >
+  <p>
+    <img :src="pokemon.image"/>
+    <br>Un {{pokemon.name}} ha aparecido<br>
+    El id del pokemon es: {{pokemon.id}}<br>
+    ¡Cuidado!, {{pokemon.name}} ha usado: {{pokemon.move}}<br>
+  </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props:[
+    'pokemon'
+  ]
+
+}
+</script>
+
+<style>
+
+</style>
